4a5bfef260f62bff9c9c1e25e8916a34962a761a
Kristof <mrmaderat1@gmail.com>
2025-05-20 22:32:58 +0300

    Remove forge, neoforge

--- forge/src/main/java/net/lionarius/skinrestorer/forge/platform/ForgeCompatibilityHelper.java
+++
@@ -1,6 +0,0 @@
-package net.lionarius.skinrestorer.forge.platform;
-
-import net.lionarius.skinrestorer.platform.services.CompatibilityHelper;
-
-public final class ForgeCompatibilityHelper implements CompatibilityHelper {
-}
